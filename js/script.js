9ccee727e1958da0ed1215e6b112ac622f81e1de
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-01-09 20:55:48 +0100

    added javascript of first exercise

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
 console.log ('JS OK')
 
+// - 1
+let userWord = prompt("Inserisci una parola:");
 
+// - 2
+const result = isPalindrome(userWord);
+
+console.log(result);
+
+// - 3
+function isPalindrome(text) {
+
+    // -4
+    text = text.toLowerCase();
+
+    console.log(text)
+
+    // - 5
+    if (text === text.split('').reverse().join('')) {
+        return "La parola è palindroma!";
+    } else {
+        return "La parola non è palindroma.";
+    }
+}
