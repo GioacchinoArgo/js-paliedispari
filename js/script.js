f7c16230df9905b6ed85702ccc3f32d6aadf440a
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-01-09 22:17:35 +0100

    added javascript of second exercise

--- js/script.js
+++ js/script.js
@@ -27,4 +27,62 @@ function isPalindrome(text) {
 }
 
 
+// ----- Secondo Esercizio -----
 
+
+// - 1
+const userNumber = parseInt(prompt("Scegli un numero da 1 a 5:"));
+const guess = prompt("Scegli se pari o dispari:");
+
+console.log(`Hai scelto: ${userNumber}`)
+console.log(`Hai scelto: ${guess}`)
+
+// - 2
+const cpuNumber = randomNumber()
+
+function randomNumber () {
+    return Math.floor(Math.random() * 5) + 1;
+}
+
+console.log(`il computer ha fatto: ${cpuNumber}`)
+
+// - 3
+const sum = sumNumber(userNumber, cpuNumber)
+
+function sumNumber (a, b) {
+    return a + b;
+}
+
+console.log (`la somma dei numeri è: ${sum}`)
+
+// - 4
+
+const odd = isOdd(sum) 
+
+function isOdd (num) {
+    if (num % 2 === 0) {
+        return true;
+
+    } else {
+        return false;
+    }
+}
+
+if (odd) {
+    console.log(`La somma dei due numeri è: pari`)
+} else {
+    console.log(`La somma dei due numeri è: dispari`)
+}
+
+// - 5
+
+switch (guess) {
+    case "pari":
+        if (odd) console.log("ha vinto l'utente!")
+        else console.log('ha vinto il computer!')
+        break;
+    case "dispari":
+        if (!odd) console.log("ha vinto l'utente!")
+        else console.log('ha vinto il computer!')
+        break;
+}
